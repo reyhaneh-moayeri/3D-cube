e51f611bae4d981889016ba64dccfdbe4e80f229
reyhaneh <reyhanehrm333@gmail.com>
2021-09-28 04:17:05 +0330

    used OrbitControls to control the camera movment

--- src/script.js
+++ src/script.js
@@ -1,15 +1,16 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // cursor
-const cursor = {
-  x: 0,
-  y: 0,
-};
-window.addEventListener("mousemove", (e) => {
-  cursor.x = e.clientX / size.width - 0.5;
-  cursor.y = -(e.clientY / size.height - 0.5);
-});
+// const cursor = {
+//   x: 0,
+//   y: 0,
+// };
+// window.addEventListener("mousemove", (e) => {
+//   cursor.x = e.clientX / size.width - 0.5;
+//   cursor.y = -(e.clientY / size.height - 0.5);
+// });
 
 const canvas = document.querySelector(".webgl");
 const scene = new THREE.Scene();
@@ -18,42 +19,8 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const mterial = new THREE.MeshBasicMaterial({ color: "orange" });
 
 const cube = new THREE.Mesh(geometry, mterial);
-
-// cube.position.z = -3;
-// cube.position.x = 1;
-// cube.position.y = -1;
-
-// cube.scale.x = 2;
-
-// cube.rotation.x = 1;
-// cube.rotation.y = 1;
 scene.add(cube);
 
-// const group = new THREE.Group();
-// scene.add(group);
-
-// const cube1 = new THREE.Mesh(
-//   new THREE.BoxGeometry(1, 1, 1),
-//   new THREE.MeshBasicMaterial({ color: "yellow" })
-// );
-// group.add(cube1);
-// const cube2 = new THREE.Mesh(
-//   new THREE.BoxGeometry(1, 1, 1),
-//   new THREE.MeshBasicMaterial({ color: "orangered" })
-// );
-// cube1.position.z = -3;
-
-// group.add(cube2);
-// cube2.position.x = -2;
-
-// const cube3 = new THREE.Mesh(
-//   new THREE.BoxGeometry(1, 1, 1),
-//   new THREE.MeshBasicMaterial({ color: "pink" })
-// );
-// cube3.position.x = 2;
-// group.add(cube3);
-
-// group.position.y = 1;
 // axes helper
 // const axesHelper = new THREE.AxesHelper();
 // scene.add(axesHelper);
@@ -83,22 +50,27 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(size.width, size.height);
 
-const clock = new THREE.Clock();
+// const clock = new THREE.Clock();
+
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 const loop = () => {
   // animate
-  const elapsedTime = clock.getElapsedTime();
+  // const elapsedTime = clock.getElapsedTime();
   // group.rotation.x += 0.001 * delta;
   // group.position.x = Math.sin(elapsedTime);
   // group.position.y = Math.sin(elapsedTime);
   // group.rotation.x = elapsedTime;
   // camera.lookAt(group.position);
 
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
-  camera.position.y = cursor.y * 3;
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+  // camera.position.y = cursor.y * 5;
 
   camera.lookAt(new THREE.Vector3());
+  controls.update();
   // render
   renderer.render(scene, camera);
   window.requestAnimationFrame(loop);
